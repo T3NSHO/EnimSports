0d58619fc6247903cf9ec80e11f9e011aa36c8b5
T3NSHO <amineos726@gmail.com>
2025-01-02 22:12:24 +0100

    final fix

---
+++ app/dashboard/generate_brackets/[tournamentIDs]/jointeam.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+interface JoinTeamProps {
+  tournamentID: string;
+}
+
+const JoinTeam = ({ tournamentID }: JoinTeamProps) => {
+  const router = useRouter();
+  const [status, setStatus] = useState('Processing...');
+
+  useEffect(() => {
+    if (tournamentID) {
+      fetch(`/api/generate_the_brackets/${tournamentID}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => {
+          if (res.ok) {
+            setStatus('Success! You have created the brackets.');
+            setTimeout(() => router.push('/dashboard'), 3000);
+          } else {
+            res.text().then((text) => setStatus(JSON.parse(text).error));
+          }
+        })
+        .catch(() => {
+          setStatus('An error occurred. Please try again later.');
+        });
+    }
+  }, [tournamentID, router]);
+
+  return (
+    <div>
+      <p>{status}</p>
+    </div>
+  );
+}
+
+
+export default JoinTeam;
--- app/dashboard/generate_brackets/[tournamentIDs]/page.tsx
+++ app/dashboard/generate_brackets/[tournamentIDs]/page.tsx
@@ -1,5 +1,5 @@
 // page.tsx
-'use client'
+import JoinTeam from '@/app/dashboard/generate_brackets/[tournamentIDs]/jointeam';
 
 interface PageProps {
   params: {
@@ -7,51 +7,7 @@ interface PageProps {
   };
 }
 
-export async function Page({ params }: PageProps) {
+export default async function Page({ params }: PageProps) {
   return <JoinTeam tournamentID={params.tournamentIDs} />;
 }
 
-// JoinTeam.tsx
-
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-
-interface JoinTeamProps {
-  tournamentID: string;
-}
-
-const JoinTeam = ({ tournamentID }: JoinTeamProps) => {
-  const router = useRouter();
-  const [status, setStatus] = useState('Processing...');
-
-  useEffect(() => {
-    if (tournamentID) {
-      fetch(`/api/generate_the_brackets/${tournamentID}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((res) => {
-          if (res.ok) {
-            setStatus('Success! You have created the brackets.');
-            setTimeout(() => router.push('/dashboard'), 3000);
-          } else {
-            res.text().then((text) => setStatus(JSON.parse(text).error));
-          }
-        })
-        .catch(() => {
-          setStatus('An error occurred. Please try again later.');
-        });
-    }
-  }, [tournamentID, router]);
-
-  return (
-    <div>
-      <p>{status}</p>
-    </div>
-  );
-}
-
-export default JoinTeam;
