62723c458703bd373733ea7d67d0f9344e671d06
T3NSHO <amineos726@gmail.com>
2025-01-02 22:16:25 +0100

    trying async now

--- app/dashboard/generate_brackets/[tournamentIDs]/page.tsx
+++ app/dashboard/generate_brackets/[tournamentIDs]/page.tsx
@@ -1,13 +1,10 @@
-// page.tsx
 import JoinTeam from '@/app/dashboard/generate_brackets/[tournamentIDs]/jointeam';
 
-interface PageProps {
-  params: {
-    tournamentIDs: string;
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  return <JoinTeam tournamentID={params.tournamentIDs} />;
-}
-
+export default async function Page({
+    params,
+  }: {
+    params: { tournamentIDs: string };
+  }) {
+    return <JoinTeam tournamentID={params.tournamentIDs} />;
+  }
+  
