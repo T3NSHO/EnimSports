bf5b2c587e1516e512460830ea58647175128792
T3NSHO <amineos726@gmail.com>
2025-01-02 18:27:17 +0100

    maybe its not meant to be

--- app/dashboard/generate_brackets/[tournamentIDs]/page.tsx
+++ app/dashboard/generate_brackets/[tournamentIDs]/page.tsx
@@ -3,38 +3,42 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
-export default function JoinTeam({ params }: { params: { tournamentIDs: string } }) {
-    const router = useRouter();
-    const { tournamentIDs } = params; // Directly destructuring params
-    const [status, setStatus] = useState('Processing...');
+interface JoinTeamProps {
+  params: {
+    tournamentIDs: string;
+  };
+}
 
-    useEffect(() => {
-        if (tournamentIDs) {
-            fetch(`/api/generate_the_brackets/${tournamentIDs}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-            )
-                .then((res) => {
-                    if (res.ok) {
-                        setStatus('Success! You have created the brackets.');
-                        setTimeout(() => router.push('/dashboard'), 3000); // Redirect after 3 seconds
-                    } else {
-                        res.text().then((text) => setStatus(JSON.parse(text).error));   
-                    }
-                })
-                .catch(() => {
-                    setStatus('An error occurred. Please try again later.');
-                });
-        }
-    }, [tournamentIDs, router]);
+export default function JoinTeam({ params }: JoinTeamProps) {
+  const router = useRouter();
+  const { tournamentIDs } = params; // Destructure `params` correctly
+  const [status, setStatus] = useState('Processing...');
 
-    return (
-        <div>
-            
-            <p>{status}</p>
-        </div>
-    );
+  useEffect(() => {
+    if (tournamentIDs) {
+      fetch(`/api/generate_the_brackets/${tournamentIDs}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => {
+          if (res.ok) {
+            setStatus('Success! You have created the brackets.');
+            setTimeout(() => router.push('/dashboard'), 3000); // Redirect after 3 seconds
+          } else {
+            res.text().then((text) => setStatus(JSON.parse(text).error));
+          }
+        })
+        .catch(() => {
+          setStatus('An error occurred. Please try again later.');
+        });
+    }
+  }, [tournamentIDs, router]);
+
+  return (
+    <div>
+      <p>{status}</p>
+    </div>
+  );
 }
